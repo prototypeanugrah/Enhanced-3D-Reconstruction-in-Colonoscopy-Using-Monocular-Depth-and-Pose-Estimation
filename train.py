4c021706ebb683d4ce48d56540616c1604063467
prototypeanugrah <anugrah_vaishnav@student.uml.edu>
2024-10-25 19:00:07 +0000

    Updated train

--- train.py
+++ train.py
@@ -71,6 +71,7 @@ def main(
     batch_size: int,
     lr: float,
     model_size: str,
+    lora_r: int,
     epochs: int,
     use_scheduler: bool = True,
     warmup_steps: int = 500,
@@ -127,10 +128,8 @@ def main(
 
     # Set up model
     model = training_utils.DepthEstimationModule(
-        f"depth-anything/Depth-Anything-V2-{model_size}-hf",
-        lr=lr,
-        use_scheduler=use_scheduler,
-        warmup_steps=warmup_steps,
+        model_name=f"depth-anything/Depth-Anything-V2-{model_size}-hf",
+        lora_r=lora_r,
     )
 
     # Count and print parameters
@@ -138,7 +137,7 @@ def main(
     logger.info("Total parameters: %d", total_params)
     logger.info("Trainable parameters: %d", trainable_params)
 
-    custom_model_name = f"pretrained_l{lr}_e{epochs}_b{batch_size}_m{model_size}_s{1 if use_scheduler else 0}_w{warmup_steps if warmup_steps else 0}"
+    custom_model_name = f"pretrained_l{lr}_e{epochs}_b{batch_size}_m{model_size}_lora{lora_r}_w{warmup_steps if warmup_steps else 0}"
 
     # Set up TensorBoard writer
     writer = SummaryWriter(
@@ -234,138 +233,6 @@ def main(
     writer.close()
 
 
-# def main(
-#     input_path: str,
-#     output_path: str,
-#     batch_size: int,
-#     lr: float,
-#     model_size: str,
-#     epochs: int,
-#     use_scheduler: bool = False,
-#     warmup_steps: int = 500,
-# ):
-#     # Process the images and get the train and validation dataloaders
-#     train_depth, train_rgb, val_depth, val_rgb = utils.process_images(
-#         input_path,
-#     )
-
-#     train_dataloader, val_dataloader = utils.get_dataloaders(
-#         train_depth,
-#         train_rgb,
-#         val_depth,
-#         val_rgb,
-#         batch_size=batch_size,
-#     )
-
-#     # temp_dataset = CustomFrameDepthDataset(
-#     #     os.path.join(input_path, "val"),
-#     #     transform=ransform,
-#     # )
-
-#     # normalize_transform = CustomFrameDepthDataset.get_normalization_transform(
-#     #     temp_dataset
-#     # )
-
-#     # transform = transforms.Compose(
-#     #     [
-#     #         transform,
-#     #         normalize_transform,
-#     #     ]
-#     # )
-
-#     # train_dataset = CustomFrameDepthDataset(
-#     #     os.path.join(input_path, "train"),
-#     #     transform=transform,
-#     # )
-#     # else:
-
-#     # train_dataset = CustomFrameDepthDataset(
-#     #     # os.path.join(input_path, "train"),
-#     #     input_path,
-#     #     transform=transform,
-#     #     start_idx=0,
-#     #     end_idx=500,
-#     #     # max_frames=1358,
-#     # )
-#     # train_dataset = Dataset(
-#     #     input_paths=train_imgs,
-#     #     target_paths=train_maps,
-#     #     transform_input=transform_input,
-#     #     transform_target=transform_target,
-#     #     hflip=True,
-#     #     vflip=True,
-#     #     affine=False,
-#     # )
-
-#     # val_dataset = Dataset(
-#     #     input_paths=val_imgs,
-#     #     target_paths=val_maps,
-#     #     transform_input=transform_input,
-#     #     transform_target=transform_target,
-#     # )
-
-#     # val_dataset = CustomFrameDepthDataset(
-#     #     # os.path.join(input_path, "val"),
-#     #     input_path,
-#     #     transform=transform,
-#     #     start_idx=0,
-#     #     end_idx=500,
-#     #     # max_frames=1358,
-#     # )
-#     # train_dataloader = data.DataLoader(
-#     #     train_dataset,
-#     #     batch_size=batch_size,
-#     #     drop_last=True,
-#     #     shuffle=True,
-#     #     num_workers=4,
-#     # )
-#     # logger.info("Train dataloader created and processed")
-
-#     # val_dataloader = data.DataLoader(
-#     #     val_dataset,
-#     #     batch_size=batch_size,
-#     #     drop_last=False,
-#     #     shuffle=False,
-#     #     num_workers=4,
-#     # )
-#     # logger.info("Validation dataloader created and processed")
-
-#     # Set up model
-#     model = DepthEstimationModule(
-#         f"depth-anything/Depth-Anything-V2-{model_size}-hf",
-#         lr=lr,
-#         use_scheduler=use_scheduler,
-#         warmup_steps=warmup_steps,
-#     )
-
-#     custom_model_name = f"pretrained_l{lr}_e{epochs}_b{batch_size}_m{model_size}_s{1 if use_scheduler else 0}_w{warmup_steps if warmup_steps else 0}"
-
-#     # Set up TensorBoard logger
-#     tb_logger = TensorBoardLogger(
-#         "tb_logs",
-#         name="depth_estimation",
-#         version=custom_model_name,
-#     )
-
-#     # Set up trainer
-#     trainer = pl.Trainer(
-#         max_epochs=epochs,
-#         accelerator="gpu" if torch.cuda.is_available() else "cpu",
-#         devices=1,
-#         logger=tb_logger,
-#         callbacks=[pl.callbacks.ModelCheckpoint(monitor="val_loss")],
-#         accumulate_grad_batches=2,
-#         log_every_n_steps=1,
-#     )
-
-#     # Train the model
-#     trainer.fit(
-#         model,
-#         train_dataloaders=train_dataloader,
-#         val_dataloaders=val_dataloader,
-#     )
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Fine-tune DepthAnythingV2 model on custom video dataset",
@@ -374,14 +241,16 @@ if __name__ == "__main__":
         "-i",
         "--input_dir",
         type=str,
-        required=True,
+        # required=True,
+        default="datasets/",
         help="Path to the directory containing input videos",
     )
     parser.add_argument(
         "-o",
         "--output_dir",
         type=str,
-        required=True,
+        # required=True,
+        default="finetuning/",
         help="Path to save the fine-tuned model",
     )
     parser.add_argument(
@@ -395,16 +264,23 @@ if __name__ == "__main__":
         "-b",
         "--batch_size",
         type=int,
-        default=4,
+        default=64,
         help="Batch size for training",
     )
     parser.add_argument(
         "-m",
         "--model_size",
         type=str,
-        default="small",
+        default="Base",
         choices=["Small", "Base", "Large"],
         help="Size of the DepthAnythingV2 model to use",
+    )
+    parser.add_argument(
+        "-lora_r",
+        "--lora_rank",
+        type=int,
+        default=4,
+        help="Rank of LoRA",
     )
     parser.add_argument(
         "-l",
@@ -415,7 +291,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "-s",
-        "--use_scheduler",
+        "--scheduler",
         type=str,
         default="True",
         help="Learning Rate Scheduler",
@@ -438,7 +314,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Convert use_scheduler to boolean
-    use_scheduler = str2bool(args.use_scheduler)
+    scheduler = str2bool(args.scheduler)
 
     os.makedirs(args.output_dir, exist_ok=True)  # Ensure the output directory exists
     os.makedirs(args.logdir, exist_ok=True)  # Ensure the logging directory exists
@@ -449,8 +325,9 @@ if __name__ == "__main__":
         args.batch_size,
         args.learning_rate,
         args.model_size,
+        args.lora_rank,
         args.epochs,
-        use_scheduler,
+        scheduler,
         args.warmup_steps,
         args.logdir,
     )
